9e5a5b2cfeb785627f7b75d2542c19c428798859
Viv Lim <vivlim@pm.me>
2022-11-13 00:04:42 -0800

    initial attempt at snoot.tube styling.
    contains theme-related commits between 7ddcaa1-39f61da.
    omits block list import logging change dcd559f

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -21,7 +21,7 @@ module Mastodon
     end
 
     def suffix
-      "+glitch#{ENV.fetch('MASTODON_VERSION_SUFFIX', '')}"
+      "+glitch-snoot#{ENV.fetch('MASTODON_VERSION_SUFFIX', '')}"
     end
 
     def to_a
@@ -33,7 +33,7 @@ module Mastodon
     end
 
     def repository
-      ENV.fetch('GITHUB_REPOSITORY', 'glitch-soc/mastodon')
+      ENV.fetch('GITHUB_REPOSITORY', 'vivlim/glitch-soc-snoottube')
     end
 
     def source_base_url
