3dc252eee787803545c8ca85254ba647e4f5e4ef
Viv Lim <vivlim@pm.me>
2023-08-24 10:03:14 -0700

    Revert "Reject remote attachments from users that aren't directly followed by a local user"
    This reverts commit 857fac650c46e946960918a77fcf360372ed58b8.

--- app/javascript/flavours/glitch/components/attachment_list.jsx
+++ app/javascript/flavours/glitch/components/attachment_list.jsx
@@ -32,14 +32,13 @@ export default class AttachmentList extends ImmutablePureComponent {
         <ul className='attachment-list__list'>
           {media.map(attachment => {
             const displayUrl = attachment.get('remote_url') || attachment.get('url');
-            const description = attachment.get('description');
 
             return (
               <li key={attachment.get('id')}>
                 <a href={displayUrl} target='_blank' rel='noopener noreferrer'>
                   {compact && <Icon id='link' />}
                   {compact && ' ' }
-                  {description ? description : displayUrl ? filename(displayUrl) : <FormattedMessage id='attachments_list.unprocessed' defaultMessage='(unprocessed)' />}
+                  {displayUrl ? filename(displayUrl) : <FormattedMessage id='attachments_list.unprocessed' defaultMessage='(unprocessed)' />}
                 </a>
               </li>
             );
